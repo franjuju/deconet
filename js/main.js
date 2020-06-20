f374a87da315686b386eb95946ddfe22c22e40bb
franjuju <julien_elkaim@hotmail.fr>
2020-06-20 16:22:18 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
 $(".emotion-pin").click(function (){
-  // if ($(".emotion-pin").siblings().hasClass("active")) {
-  //   $(".emotion-pin").siblings().removeClass("active")
-  //   $(this).siblings().addClass("active")
-  //   // $(".emotion-pin").not(this).css('opacity','0.7')
-  // } else {
-  //   // $(".emotion-pin").not(this).css('opacity','0.7')
-  //   $(this).siblings().addClass("active")
-  // }
   if($(".emotion-pin").siblings().hasClass("active")) {
     $(".emotion-pin").siblings().removeClass("active")
     $(this).siblings().addClass("active")
   } else {
     $(this).siblings().addClass("active")
   }
-  
 })
 
 $(".close").click(function (){
   $(".emotion-pin").siblings().removeClass("active")
+})
+
+var multiple = new Multiple({
+  selector: '.jauge-detail',
+  background: 'linear-gradient(to right, #FF0066, #33CCFF)'
+});
+
+$(".button-reduce").click(function(){
+   if (!$(".jauge6").children().hasClass("active")){
+    $(".jauge6").children().addClass("active")
+  } else if (!$(".jauge5").children().hasClass("active")){
+    $(".jauge5").children().addClass("active")
+  } else if (!$(".jauge4").children().hasClass("active")){
+    $(".jauge4").children().addClass("active")
+  } else if (!$(".jauge3").children().hasClass("active")){
+    $(".jauge3").children().addClass("active")
+  } else if (!$(".jauge2").children().hasClass("active")){
+    $(".jauge2").children().addClass("active")
+  } else if (!$(".jauge1").children().hasClass("active")){
+    $(".jauge1").children().addClass("active")
+  } 
+})
+$(".button-add").click(function(){
+  if ($(".jauge1").children().hasClass("active")){
+    $(".jauge1").children().removeClass("active")
+  } else if ($(".jauge2").children().hasClass("active")){
+    $(".jauge2").children().removeClass("active")
+  } else if ($(".jauge3").children().hasClass("active")){
+    $(".jauge3").children().removeClass("active")
+  } else if ($(".jauge4").children().hasClass("active")){
+    $(".jauge4").children().removeClass("active")
+  } else if ($(".jauge5").children().hasClass("active")){
+    $(".jauge5").children().removeClass("active")
+  } else if ($(".jauge6").children().hasClass("active")){
+    $(".jauge6").children().removeClass("active")
+  } 
 })
--- search-result.php
+++ search-result.php
@@ -31,9 +31,38 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style-pauline.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/multiple.js/0.0.1/multiple.min.css" integrity="sha256-8V5YphE5KeZqON9KOAVgvoqUG+cUCNGicW2wEB4gwaU=" crossorigin="anonymous" />
   </head>
     <body>
+      <a href="brain.html"><img class="close-search" src="img/close.png"></a>
       <h1><?php foreach ($data as $element) {echo $element['name'];} ?></h1>
       <p class="description-emotion"><?php foreach ($data as $element) {echo $element['description'];} ?></p>
+      <div class="jauge">
+        <div class="jauge-detail jauge1">
+          <div class="hider"></div>
+        </div>
+        <div class="jauge-detail jauge2">
+          <div class="hider"></div>
+        </div>
+        <div class="jauge-detail jauge3">
+          <div class="hider"></div>
+        </div>
+        <div class="jauge-detail jauge4">
+          <div class="hider active"></div>
+        </div>
+        <div class="jauge-detail jauge5">
+          <div class="hider active"></div>
+        </div>
+        <div class="jauge-detail jauge6">
+          <div class="hider active"></div>
+        </div>
+      </div>
+      <div class="cta-container">
+        <a class="button-reduce" href="#">Reduce emotion</a>
+        <a class="button-add" href="#">Add emotion</a>
+      </div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/multiple.js/0.0.1/multiple.min.js" integrity="sha256-4+vj4FcENMARxlfF1mJhD1UoK8TQDUKRoFtJO+Z0TIU=" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
 </html>
